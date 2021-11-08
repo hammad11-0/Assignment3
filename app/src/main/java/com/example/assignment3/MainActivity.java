945e7b3ef72335be341f23710b4b8c2125893aba
hammad11-0 <bitf18a504@pucit.edu.pk>
2021-11-08 21:11:39 +0500

    1st activity completed

--- app/src/main/java/com/example/assignment3/MainActivity.java
+++ app/src/main/java/com/example/assignment3/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.assignment3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    String url="https://github.com/hammad11-0/Assignment3";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+     b1=findViewById(R.id.button);
+
+    b1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+                openWebPage(url);
+        }
+    });
+
+
+    }
+
+    public View.OnClickListener openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new
+                Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+        return null;
     }
 }
