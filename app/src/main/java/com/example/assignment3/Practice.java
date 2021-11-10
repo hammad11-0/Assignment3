6f886684421386c829ffafde0e9cc88c1a5f0dfa
hammad11-0 <bitf18a504@pucit.edu.pk>
2021-11-10 23:27:57 +0500

    lahatiyah added in practice

--- app/src/main/java/com/example/assignment3/Practice.java
+++ app/src/main/java/com/example/assignment3/Practice.java
@@ -9,12 +9,13 @@ import android.widget.Button;
 
 public class Practice extends AppCompatActivity {
 
-    Button b1;
+    Button b1,b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
         b1.findViewById(R.id.halq);
+        b2.findViewById(R.id.Lahat);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,6 +23,16 @@ public class Practice extends AppCompatActivity {
                 Intent intent;
 
                 intent = new Intent(Practice.this, halqiyah.class);
+                startActivity(intent);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+
+                intent = new Intent(Practice.this,lahatiyah.class);
                 startActivity(intent);
             }
         });
