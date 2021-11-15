c8f72228fdfa6c32fb859a5b55d7ab958b67af1d
hammad11-0 <bitf18a504@pucit.edu.pk>
2021-11-16 00:01:22 +0500

    exam added

--- app/src/main/java/com/example/assignment3/Exam.java
+++ app/src/main/java/com/example/assignment3/Exam.java
@@ -2,12 +2,19 @@ package com.example.assignment3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class Exam extends AppCompatActivity {
 
     TextView t1;
+        String n;
+        Button b1,b2,b3,b4,b5,b6,b7;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +30,127 @@ public class Exam extends AppCompatActivity {
         String[] ghunna = {"م","ن", "ف","ب","م","و","باَ","بوُ","بىِ"};
 
 
-  t1=findViewById(R.id.textView20);
-  t1.setText(num[1]);
+  t1=findViewById(R.id.textView2);
+
+        b1=findViewById(R.id.HALQIYAH);
+        b2=findViewById(R.id.button8);
+        b3=findViewById(R.id.button9);
+        b4=findViewById(R.id.button10);
+        b5=findViewById(R.id.button11);
+        b6=findViewById(R.id.button12);
+        b7=findViewById(R.id.button13);
+
+
+        Random rand = new Random();
+
+     int a= rand.nextInt(32);
+
+    n=num[a];
+    int id=1;
+  t1.setText(n);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < 6; i++) {
+                    if (alqiyah[i].equals(n)) {
+                        b1.setBackgroundColor(Color.GREEN);
+                        break;
+                    } else {
+                        b1.setBackgroundColor(Color.RED);
+                    }
+                  }
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < 2; i++) {
+                    if (lahatiyah[i].equals(n)) {
+                        b2.setBackgroundColor(Color.GREEN);
+                        break;
+                    } else {
+                        b2.setBackgroundColor(Color.RED);
+                    }
+                }
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < 4; i++) {
+                    if (shajariyah_haafiyah[i].equals(n)) {
+                        b3.setBackgroundColor(Color.GREEN);
+                        break;
+                    } else {
+                        b3.setBackgroundColor(Color.RED);
+                    }
+                }
+            }
+        });
+
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < 3; i++) {
+                    if (tarfiyah[i].equals(n)) {
+                        b4.setBackgroundColor(Color.GREEN);
+                        break;
+                    } else {
+                        b4.setBackgroundColor(Color.RED);
+                    }
+                }
+            }
+        });
+
+
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < 3; i++) {
+                    if (nit_eeyah[i].equals(n)) {
+                        b5.setBackgroundColor(Color.GREEN);
+                        break;
+                    } else {
+                        b5.setBackgroundColor(Color.RED);
+                    }
+                }
+            }
+        });
+
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < 6; i++) {
+                    if (lisaveyah[i].equals(n)) {
+                        b6.setBackgroundColor(Color.GREEN);
+                        break;
+                    } else {
+                        b6.setBackgroundColor(Color.RED);
+                    }
+                }
+            }
+        });
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < 9; i++) {
+                    if (ghunna[i].equals(n)) {
+                        b7.setBackgroundColor(Color.GREEN);
+                        break;
+                    } else {
+                        b7.setBackgroundColor(Color.RED);
+                    }
+                }
+            }
+        });
     }
 }
+
+
+
+
+
+
